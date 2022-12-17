1b14269ce838c7752db6f477c160e1421a4696f5
Lucas Allison <lucasgallison@gmail.com>
2022-12-17 13:56:56 +0100

    added gitignore, changed parameters of bv graph

--- src/social_network_compression/compression.java
+++ src/social_network_compression/compression.java
@@ -10,12 +10,13 @@ class compression {
 	
 	public static void main(final String args[]) throws IOException {
 
-		String baseName = "/home/lucas/cnr-2000";
-		String comprName = "/home/lucas/out.compressed";
-		Boolean useBacklinks = true;
+		String baseName = "/home/lucas/Github/snacs/webgraph/src/social_network_compression/graphs/slashdot/slashdot-converted.graph-txt";
+		String comprName = "/home/lucas/Github/snacs/webgraph/src/social_network_compression/graphs/slashdot/slashdot.compressed";
 		ASCIIGraph inGraph = ASCIIGraph.loadOffline(baseName);
 		ArrayListMutableGraph graph = new ArrayListMutableGraph(inGraph); // by copying to a mutable graph, random access is possible
-		BVGraph.store(graph.immutableView(), comprName, -1, BVGraph.DEFAULT_MAX_REF_COUNT, useBacklinks ? BVGraph.NO_INTERVALS : BVGraph.DEFAULT_MIN_INTERVAL_LENGTH, -1, 1, 1, null);
+		int windowSize = 4;
+		int referenceCount = BVGraph.DEFAULT_MAX_REF_COUNT;
+		BVGraph.store(graph.immutableView(), comprName, windowSize, referenceCount, BVGraph.DEFAULT_MIN_INTERVAL_LENGTH, -1, 1, 0, null);
 	
 	}
 }
