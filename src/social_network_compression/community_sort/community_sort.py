9d37665919162c0af9d121c4c79daa83a5a40b7c
Lucas Allison <lucasgallison@gmail.com>
2022-12-18 12:44:32 +0100

    moved to sub dir

--- src/social_network_compression/community_sort/community_sort.py
+++ src/social_network_compression/community_sort/community_sort.py
@@ -7,24 +7,19 @@ import igraph as ig
 import leidenalg as la
 from queue import PriorityQueue
 
-#http://konect.cc/networks/twitter_mpi/
-
 fh=open('out.ego-twitter') # adjust file here.
 G=nx.read_weighted_edgelist(fh,comments='%',delimiter='\t',create_using=nx.DiGraph) # check the delimiter in file and adjust if necesary
 size = G.number_of_nodes()
 G = ig.Graph.from_networkx(G)
-#print(G)
 partition = la.find_partition(G, la.ModularityVertexPartition)
 
-#print(partition)
-#per cominity the node is orderd on degree.
+# per cominity the node is orderd on degree.
 sort_part=[]
 for i in range(len(partition)):
 	tempy=PriorityQueue()
 	for j in range(len(partition[i])):
 		d = G.degree(partition[i][j],mode="out")
 		tempy.put((d*-1,partition[i][j]))
-		#print(j)
 	sort_part.append(tempy)
 	
 #convertion for new order perpaird.
@@ -34,7 +29,6 @@ i=0
 for element in sort_part:
 	while not element.empty():
 		next_item, node = element.get()
-		#print(node)
 		convert[node]=i
 		order.append(node)
 		i=i+1
@@ -57,9 +51,3 @@ with open(r'data.txt', 'w') as fp:
 	for element in neig:
 		print(element)
 		fp.write("%s\n" % ' '.join(map(str,element)))
-
-
-
-
-#465017 big twitter
-#23,370 small twitter
